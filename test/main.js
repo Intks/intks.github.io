b9708b3e8bcaa7a007a24e5d67dffbacaecdbd82
Alex <shift1255@gmail.com>
2020-02-27 17:30:31 +0800

    update

--- test/main.js
+++ test/main.js
@@ -1,11 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-import AlexUI from '../packages/index.js'
-
-Vue.use(AlexUI)
+import AlexUI from '../packages'
 
 Vue.config.productionTip = false
+
+Vue.use(AlexUI)
 
 new Vue({
   render: h => h(App)
